e34f365fad06f39e4727b6adab0c6c9c356b182e
jonathanhhoward <2773262@gmail.com>
2020-04-22 00:37:03 -0400

    Convert lambda to function

--- model.js
+++ model.js
@@ -2,13 +2,13 @@
 
 const collection = require('./db')
 
-module.exports.add = async (symbol, ip) => {
+module.exports.add = async function (symbol, ip) {
   const db = await collection(symbol)
   const result = await db.findOne({ ip: ip })
   if (!result) await db.insertOne({ ip: ip })
 }
 
-module.exports.count = async (symbol) => {
+module.exports.count = async function (symbol) {
   const db = await collection(symbol)
   const cursor = db.find({})
   return await cursor.count()
