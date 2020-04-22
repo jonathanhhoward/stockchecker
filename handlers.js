fd59fc46704f077f48aaf3d3b617694f6d4aaea0
jonathanhhoward <2773262@gmail.com>
2020-04-22 00:49:17 -0400

    Replace lambda with function

--- handlers.js
+++ handlers.js
@@ -3,23 +3,21 @@
 const fetch = require('node-fetch')
 const likes = require('./model')
 
-module.exports = {
-  getPrices: async (req, res, next) => {
-    const { stock: symbol, like } = req.query
-    const url = `https://repeated-alpaca.glitch.me/v1/stock/${symbol}/quote`
-    try {
-      const response = await fetch(url)
-      const data = await response.json()
-      if (like) await likes.add(symbol, req.ip)
-      res.json({
-        stockData: {
-          stock: data.symbol,
-          price: data.latestPrice,
-          likes: await likes.count(symbol)
-        }
-      })
-    } catch (error) {
-      next(error)
-    }
+module.exports.getPrices = async function (req, res, next) {
+  const { stock: symbol, like } = req.query
+  const url = `https://repeated-alpaca.glitch.me/v1/stock/${symbol}/quote`
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    if (like) await likes.add(symbol, req.ip)
+    res.json({
+      stockData: {
+        stock: data.symbol,
+        price: data.latestPrice,
+        likes: await likes.count(symbol)
+      }
+    })
+  } catch (error) {
+    next(error)
   }
 }
