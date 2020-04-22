39346ce6c7210d1145a98ddce116eb7e71e21663
jonathanhhoward <2773262@gmail.com>
2020-04-21 23:37:13 -0400

    Retrieve symbol and latestPrice

--- handlers.js
+++ handlers.js
@@ -5,9 +5,18 @@ const model = require('./model')
 
 module.exports = {
   getPrices: async (req, res, next) => {
+    const symbol = req.query.stock
+    const url = `https://repeated-alpaca.glitch.me/v1/stock/${symbol}/quote`
+    const response = await fetch(url)
     try {
-      const result = await model.test()
-      res.json(result)
+      const data = await response.json()
+      res.json({
+        stockData: {
+          stock: data.symbol,
+          price: data.latestPrice,
+          likes: 0
+        }
+      })
     } catch (error) {
       next(error)
     }
