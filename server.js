e4c23acf07a8927245e8cba992bd40a486ec8d59
jonathanhhoward <2773262@gmail.com>
2020-04-24 18:13:26 -0400

    Add helmet

--- server.js
+++ server.js
@@ -5,12 +5,18 @@ require('dotenv').config()
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const helmet = require('helmet')
 
 const apiRoutes = require('./routes/api-routes.js')
 const fccTestingRoutes = require('./routes/fcctesting.js')
 const runner = require('./test-runner')
 
 const app = express()
+
+app.use(helmet())
+app.use(helmet.contentSecurityPolicy({
+  directives: { defaultSrc: ['\'self\''] }
+}))
 
 app.use('/public', express.static(`${__dirname}/public`))
 
