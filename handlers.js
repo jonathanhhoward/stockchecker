8410c142ed79933b8519e4781abe0a6fbeaea17d
jonathanhhoward <2773262@gmail.com>
2020-04-23 21:25:57 -0400

    Extract constant bothStocksAreValid. Extract function makeLikesRelative.

--- handlers.js
+++ handlers.js
@@ -11,12 +11,8 @@ module.exports.getPrices = async function (req, res, next) {
     if (Array.isArray(stock)) {
       const stock0 = await getStockData(stock[0], like, ip)
       const stock1 = await getStockData(stock[1], like, ip)
-      if (!stock0.error && !stock1.error) {
-        stock0.rel_likes = stock0.likes - stock1.likes
-        stock1.rel_likes = stock1.likes - stock0.likes
-        delete stock0.likes
-        delete stock1.likes
-      }
+      const bothStocksAreValid = !stock0.error && !stock1.error
+      if (bothStocksAreValid) makeLikesRelative(stock0, stock1)
       res.json({ stockData: [stock0, stock1] })
     } else {
       res.json({ stockData: await getStockData(stock, like, ip) })
@@ -41,4 +37,11 @@ async function fetchStock (symbol) {
   const url = `https://repeated-alpaca.glitch.me/v1/stock/${symbol}/quote`
   const res = await fetch(url)
   return res.json()
+}
+
+function makeLikesRelative (stock0, stock1) {
+  stock0.rel_likes = stock0.likes - stock1.likes
+  stock1.rel_likes = stock1.likes - stock0.likes
+  delete stock0.likes
+  delete stock1.likes
 }
