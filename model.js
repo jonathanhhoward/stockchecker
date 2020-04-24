ba8beda70442f0e8e7550b5ecb5566c43a9fdd90
jonathanhhoward <2773262@gmail.com>
2020-04-24 17:53:42 -0400

    Remove module keyword

--- model.js
+++ model.js
@@ -2,13 +2,13 @@
 
 const collection = require('./db')
 
-module.exports.add = async function (symbol, ip) {
+exports.add = async function (symbol, ip) {
   const db = await collection(symbol)
   const result = await db.findOne({ ip: ip })
   if (!result) await db.insertOne({ ip: ip })
 }
 
-module.exports.count = async function (symbol) {
+exports.count = async function (symbol) {
   const db = await collection(symbol)
   const cursor = db.find({})
   return cursor.count()
